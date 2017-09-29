bec770072e929611cf382419aec15f6352847406
nikolaev140591 <32127645+nikolaev140591@users.noreply.github.com>
2017-09-29 11:19:23 +0300

    new test

--- addressbook-web-tests/src/test/java/evg/tests/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/evg/tests/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase{
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("Тестовая", "тест2", "тест3"));
+    app.getGroupHelper().fillGroupForm(new GroupData("Тестовая111", "тест2222", "тест3333"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
   }
